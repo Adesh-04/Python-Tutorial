96d2d6da9276313f7605a0c3171eee11ce8e5371
Adesh <adesh@Kali>
2022-07-24 15:22:05 +0530

    bot.py

---
+++ Bots/Piano_Tiles.py
@@ -0,0 +1,23 @@
+ 
+import pyautogui
+# import keyboard
+
+## ( 1 , 1 , 1 )
+# X: 466 Y:  440 
+# X: 535 Y:  440 
+# X: 601 Y:  440 
+# X: 668 Y:  440
+
+# def click(pos_x,pos_y):
+#     pyautogui.click( 466,440 )  
+
+# while keyboard.is_pressed('q') == False:
+#     if pyautogui.pixel(466,440)[0] == 1:
+#         click(466,400)
+#     if pyautogui.pixel(535,440)[0] == 1:
+#         click(535,400)
+#     if pyautogui.pixel(601,440)[0] == 1:
+#         click(601,400)
+#     if pyautogui.pixel(668,440)[0] == 1:
+#         click(668,400)
+        
